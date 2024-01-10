8fbcaaee938f3e1feefd6bcd0cc2c07eaabff262
sunamo <radek.jancik@sunamo.cz>
2024-01-10 11:50:41 +0100

    .

---
+++ Args/DumpAsStringArgs.cs
@@ -0,0 +1,16 @@
+namespace SunamoReflection.Args;
+
+/// <summary>
+/// U��v� se v SunamoReflection+SUnamoLogger
+/// </summary>
+public class DumpAsStringArgs : DumpAsStringHeaderArgs
+{
+    public string name = string.Empty;
+    public object o;
+    public DumpProvider d = DumpProvider.Yaml;
+    /// <summary>
+    /// Good for fast comparing objects
+    /// </summary>
+    public bool onlyValues;
+    public string deli = " - ";
+}
