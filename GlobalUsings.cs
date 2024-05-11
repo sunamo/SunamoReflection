7adbfe426021cd8806c352ffe4336245f490a994
sunamo.cz_asus <radek.jancik@sunamo.cz>
2024-05-11 11:16:11 +0200

    Some projects are flat and buildable

--- GlobalUsings.cs
+++ GlobalUsings.cs
@@ -1,21 +1,4 @@
-//global using System.Collections.Generic;
-//global using System.Reflection;
-//global using System;
-//global using System.Linq;
-//global using System.Text;
-//global using System.Collections;
 
-//global using SunamoValues;
-//global using SunamoReflection.Enums;
-//global using System.Data;
-//global using SunamoArgs;
-//global using SunamoExtensions;
-//global using System.ComponentModel;
-//global using SunamoI18N;
-//global using SunamoI18N.Values;
-//global using System.Xml.Linq;
-//global using SunamoReflection.Args;
-//global using System.Collections.Immutable;
 
 global using System.Collections.Generic;
 global using System.Reflection;
@@ -33,3 +16,7 @@ global using System.Collections.Immutable;
 global using SunamoExceptions.OnlyInSE;
 
 global using SunamoExceptions;
+global using System.Collections.Generic;
+global using System.Collections;
+global using System.IO;
+
