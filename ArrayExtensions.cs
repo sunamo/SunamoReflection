c18207646023925953f3f7e33b71a88b827288da
sunamo.cz_asus <radek.jancik@sunamo.cz>
2024-06-30 13:15:18 +0200

    All buildable, every in separate file

---
+++ ArrayExtensions.cs
@@ -0,0 +1,12 @@
+﻿namespace SunamoReflection;
+
+public static class ArrayExtensions
+{
+    public static void ForEach(this Array array, Action<Array, int[]> action)
+    {
+        if (array.LongLength == 0) return;
+        ArrayTraverse walker = new ArrayTraverse(array);
+        do action(array, walker.Position);
+        while (walker.Step());
+    }
+}
