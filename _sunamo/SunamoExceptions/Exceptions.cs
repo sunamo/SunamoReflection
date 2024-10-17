2d69bb3627f38a07faef437ce36f34b5698dcb6f
sunamo.cz <radek.jancik@sunamo.cz>
2024-10-17 13:41:05 +0200

    Finished convert to refactored ThrowEx.cs

--- _sunamo/SunamoExceptions/Exceptions.cs
+++ _sunamo/SunamoExceptions/Exceptions.cs
@@ -1,14 +1,14 @@
 namespace SunamoReflection._sunamo.SunamoExceptions;
 // © www.sunamo.cz. All Rights Reserved.
-public sealed partial class Exceptions
+internal sealed partial class Exceptions
 {
     #region Other
-    public static string CheckBefore(string before)
+    internal static string CheckBefore(string before)
     {
         return string.IsNullOrWhiteSpace(before) ? string.Empty : before + ": ";
     }
 
-    public static string TextOfExceptions(Exception ex, bool alsoInner = true)
+    internal static string TextOfExceptions(Exception ex, bool alsoInner = true)
     {
         if (ex == null) return string.Empty;
         StringBuilder sb = new();
@@ -24,7 +24,7 @@ public sealed partial class Exceptions
         return r;
     }
 
-    public static Tuple<string, string, string> PlaceOfException(
+    internal static Tuple<string, string, string> PlaceOfException(
 bool fillAlsoFirstTwo = true)
     {
         StackTrace st = new();
@@ -52,7 +52,7 @@ bool fillAlsoFirstTwo = true)
         }
         return new Tuple<string, string, string>(type, methodName, string.Join(Environment.NewLine, l));
     }
-    public static void TypeAndMethodName(string l, out string type, out string methodName)
+    internal static void TypeAndMethodName(string l, out string type, out string methodName)
     {
         var s2 = l.Split("at ")[1].Trim();
         var s = s2.Split("(")[0];
@@ -61,7 +61,7 @@ bool fillAlsoFirstTwo = true)
         p.RemoveAt(p.Count - 1);
         type = string.Join(".", p);
     }
-    public static string CallingMethod(int v = 1)
+    internal static string CallingMethod(int v = 1)
     {
         StackTrace stackTrace = new();
         var methodBase = stackTrace.GetFrame(v)?.GetMethod();
@@ -75,7 +75,7 @@ bool fillAlsoFirstTwo = true)
     #endregion
 
     #region IsNullOrWhitespace
-    public static string? IsNullOrWhitespace(string before, string argName, string argValue, bool notAllowOnlyWhitespace)
+    internal static string? IsNullOrWhitespace(string before, string argName, string argValue, bool notAllowOnlyWhitespace)
     {
         string addParams;
         if (argValue == null)
@@ -97,7 +97,7 @@ bool fillAlsoFirstTwo = true)
     }
     readonly static StringBuilder sbAdditionalInfoInner = new();
     readonly static StringBuilder sbAdditionalInfo = new();
-    public static string AddParams()
+    internal static string AddParams()
     {
         sbAdditionalInfo.Insert(0, Environment.NewLine);
         sbAdditionalInfo.Insert(0, "Outer:");
@@ -112,16 +112,16 @@ bool fillAlsoFirstTwo = true)
     #endregion
 
     #region OnlyReturnString 
-    public static string? NotImplementedMethod(string before)
+    internal static string? NotImplementedMethod(string before)
     {
         return CheckBefore(before) + "Not implemented method.";
     }
     #endregion
-    public static string? IsNull(string before, string variableName, object? variable)
+    internal static string? IsNull(string before, string variableName, object? variable)
     {
         return variable == null ? CheckBefore(before) + variableName + " " + "is null" + "." : null;
     }
-    public static string? NotImplementedCase(string before, object notImplementedName)
+    internal static string? NotImplementedCase(string before, object notImplementedName)
     {
         var fr = string.Empty;
         if (notImplementedName != null)
@@ -132,7 +132,7 @@ bool fillAlsoFirstTwo = true)
             else
                 fr += notImplementedName.ToString();
         }
-        return CheckBefore(before) + "Not implemented case" + fr + " . public program error. Please contact developer" +
+        return CheckBefore(before) + "Not implemented case" + fr + " . internal program error. Please contact developer" +
         ".";
     }
 }
